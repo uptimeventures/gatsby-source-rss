feb2f281ac2d98d4eb7b956cce282e81054eb13c
Nicholas Young <nyoung@uptime.ventures>
2018-06-15 18:40:55 -0600

    feat(rss): add support for importing rss feed

---
+++ gatsby-node.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2018 Uptime Ventures, Ltd. All rights reserved.
+ * Usage of this source code is governed by a BSD-style license
+ * that can be found in LICENSE.md, at the root of this repository.
+ *
+ * @flow
+ */
+
+const { load, createFeed } = require('./internals')
+
+async function sourceNodes({ boundActionCreators }, options = {}) {
+  const { createNode } = boundActionCreators
+  const { feeds = [] } = options
+
+  for (const f of feeds) {
+    const { rss } = await load(f)
+
+    if (rss && rss.channel) {
+      const sources = (Array.isArray(rss.channel)
+        ? rss.channel : [rss.channel]
+      )
+
+      sources.forEach(f => createFeed(f, createNode))
+    }
+  }
+
+  return Promise.resolve()
+}
+
+module.exports = {
+  sourceNodes,
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * Copyright 2018 Uptime Ventures, Ltd.
+ * Released under the BSD-3-Clause License.
+ */
+
+module.exports = () => {}
